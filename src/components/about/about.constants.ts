0006d6778629f3e4ed44ba909717d7ebfa3cea5e
Danil <51288221+kyzinatra@users.noreply.github.com>
2024-05-27 18:26:02 +0300

    delete images

--- src/components/about/about.constants.ts
+++ src/components/about/about.constants.ts
@@ -1,11 +1,11 @@
 import Card1 from "../../../public/about/card-1.png";
 import Card2 from "../../../public/about/card-2.png";
 import Card3 from "../../../public/about/card-3.png";
+import Card4 from "../../../public/about/card-4.png";
+import Card5 from "../../../public/about/card-5.png";
+import Card6 from "../../../public/about/card-6.png";
 import Card7 from "../../../public/about/card-7.png";
 import Card8 from "../../../public/about/card-8.png";
-import Card9 from "../../../public/about/card-9.png";
-import Card11 from "../../../public/about/card-11.png";
-import Card12 from "../../../public/about/card-12.png";
 
 import css from "./about.module.css";
 
@@ -24,22 +24,22 @@ export const aboutCards = [
 	},
 
 	{
-		src: Card7,
+		src: Card4,
 	},
 	{
-		src: Card8,
+		src: Card5,
 		content:
 			"Мы гордимся своей репутацией, основанной на ответственности и опыте. Над проектами ежедневно трудится штат сотрудников, в котором соединились опытные профессионалы  и молодые специалисты.",
 	},
 	{
-		src: Card9,
+		src: Card6,
 		className: css.about__grid_wide,
 	},
 	{
-		src: Card11,
+		src: Card7,
 	},
 	{
-		src: Card12,
+		src: Card8,
 		content:
 			"Наши специалисты  имеют регистрацию в национальном реестре специалистов НОПРИЗ и закреплены за нашей организацией вторым уровнем ответственности в ассоциации СРО «ОПОРА» и СРО «АИИС».",
 	},
