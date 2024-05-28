e985e706b846d6753fa257de552e9b627699d865
Danil <51288221+kyzinatra@users.noreply.github.com>
2024-05-28 10:10:50 +0300

    add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()]
+	site: "https://eko-dv-proekt.ru",
+	integrations: [sitemap()],
 });
