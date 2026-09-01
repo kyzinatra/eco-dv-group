509613ca15f5b68fec3f782400e6dce370730fbc
Danil <51288221+kyzinatra@users.noreply.github.com>
2026-09-01 12:40:18 +1000

    Update contacts.constants.ts

--- src/components/contacts/contacts.constants.ts
+++ src/components/contacts/contacts.constants.ts
@@ -1,5 +1,5 @@
 export const CONTACTS = [
 	["Г. Владивосток, ул. Семеновская, 29", "(отель “ЛОТТЕ”) 6 этаж, офис 611"],
-	["+79089992121", "+79089928118", "eko-dv-proekt@mail.ru"],
-	["WhatsApp", "Telegram", "eko-dv-proekt@mail.ru"],
+	["WhatsApp", "Telegram", "Mail"],
+	["+79089992121", "+79089928118", "info@eko-dv-proekt.ru"],
 ];
